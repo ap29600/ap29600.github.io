c85c30c23b39a303abe097b68366980da76bb3a4
Andrea Piseri <andrea.piseri@gmail.com>
2026-08-26 23:34:33 +0200

    boolean scans

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 for DOCUMENT in `ls src/*.adoc`; do
 	OUTPUT="$(basename $DOCUMENT .adoc).html"
-	[ $(stat $DOCUMENT --printf="%X") -lt $(stat $OUTPUT --printf="%X") ] || asciidoctor $DOCUMENT -o $OUTPUT
+	[ $(stat $DOCUMENT --printf="%X") -lt $(stat $OUTPUT --printf="%X") ] || asciidoctor $DOCUMENT -r asciidoctor-diagram -o $OUTPUT
 done
